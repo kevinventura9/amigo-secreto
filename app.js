3fd1ea45ada7e83fbbacaae8dd3612e323da5d07
Kevin Ventura <kevin.ventura9@gmail.com>
2025-08-07 21:12:01 -0600

    Agregar funcionalidad en backend

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    if (amigo.trim() != "") {
+        listaAmigos.push(amigo);
+        document.getElementById("amigo").value = "";
+        actualizarLista();
+    } else {
+        alert("Ingrese un nombre valido");
+    }
+    return;
+}
+
+function actualizarLista() {
+    let listadoHTML = document.querySelector("#listaAmigos");
+    listadoHTML.innerHTML = "";
+    for (let contador = 0; contador < listaAmigos.length; contador++) {
+        let amigo = listaAmigos[contador];
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listadoHTML.appendChild(li);
+    }
+    return;
+}
+
+function sortearAmigo() {
+    if (listaAmigos.length > 0) {
+        let numeroRandom = Math.floor(Math.random() * listaAmigos.length);
+        amigoSorteado = listaAmigos[numeroRandom];
+        document.getElementById("resultado").innerHTML = amigoSorteado;
+        document.getElementById("listaAmigos").innerHTML = "";
+    }
+    return;
+}
+
+
+
+
